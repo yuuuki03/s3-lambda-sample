ee2e8826c83f6326173e1ffddf05601841bd0cee
ykyoshi <yuki.y319@gmail.com>
2023-03-02 04:44:09 +0900

    update
    Swagger is used for the format change.

--- README.md
+++ README.md
@@ -5,8 +5,12 @@ QueryStringにObjectPathを設定してGETリクエストを送信するとS3の
 S3バケットが１つ作成されるので、事前にObjectを格納して下さい（手動）
 格納されたObjectのパスをクエリ文字列でAPIGatewayに渡すと署名付きURLが返却されます。
 
-リクエストサンプル
-https://wsu1vvvdpj.execute-api.ap-northeast-1.amazonaws.com/Prod/req/str?pathToObj=template.yaml
+apigatewayの部分はSwagger +API Gateway 拡張の形式で記載
+
+sam build
+sam deploy --capabilities CAPABILITY_NAMED_IAM
 
 ## 参考
 https://docs.aws.amazon.com/ja_jp/sdk-for-javascript/v3/developer-guide/javascript_s3_code_examples.html
+https://zenn.dev/tabigaeru/articles/ca9b996268faf8
+https://confrage.jp/aws-api-gateway%E3%81%8B%E3%82%89lambda%E3%82%92%E9%80%9A%E3%81%95%E3%81%9A%E3%81%ABs3%E3%81%B8%E9%80%A3%E6%90%BA%E3%81%99%E3%82%8B%E6%96%B9%E6%B3%95/
--- s3-presigner/app.mjs
+++ s3-presigner/app.mjs
@@ -2,14 +2,16 @@ import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const lambdaHandler = async (event, context) => {
-    const bucketName = process.env['S3_BUCKET']
+    //const bucketName = process.env['S3_BUCKET']
     //const s3ObjectKey= process.env['S3_KEY'];
+    const bucket = event.queryStringParameters.s3Bucket;
     const path = event.queryStringParameters.pathToObj;
-    console.log('queryStringParameters: ', path);
+    console.log('queryStringParameters: ', path, bucket);
 
     const command = new GetObjectCommand({
-        Bucket: bucketName,
+        //Bucket: bucketName,
         //Key: s3ObjectKey
+        Bucket: bucket,
         Key: path
     });
     const signedUrl = await getSignedUrl(new S3Client(), command, {
